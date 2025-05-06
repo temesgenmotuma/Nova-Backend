49d73d306fd7302e095ddc15d81b17996c3bac40
temesgenmotuma <janijesus674@gmail.com>
2025-05-06 18:36:39 +0300

    Remove dist folder from version control and add to .gitignore

--- dist/Routes/router.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-exports.router = require('express').Router();
-//# sourceMappingURL=router.js.map
